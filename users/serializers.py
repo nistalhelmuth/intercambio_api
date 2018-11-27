e6319c38e59c77e63756b8e51f8beb6f80d31b78
pablogadhi <pablogadhi@gmail.com>
2018-11-27 09:53:56 -0600

    Arreglo de Hashing de los Passwords

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
 from . import models
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'age', 'phone', 'rating', 'img']
+        fields = ['id', 'username', 'password', 'first_name',
+                  'last_name', 'email', 'age', 'phone', 'rating', 'img']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+class UserSerializerNoPassword(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = ['id', 'username', 'first_name', 'last_name',
+                  'email', 'age', 'phone', 'rating', 'img']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from . import models, serializers
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.UserSerializerNoPassword
+        return serializers.UserSerializer
